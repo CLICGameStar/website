cd563b819ede760c50463ce385d689ee8d6959eb
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-08-26 17:41:27 +0200

    feat: add middleware for internationalization

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const LOCALES = ["en", "fr"];
+
+export async function middleware(request: any) {
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = LOCALES.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) {
+    return NextResponse.next();
+  }
+
+  // Redirect if there is no locale
+  let newPath = `/en${pathname}`;
+  return NextResponse.rewrite(new URL(newPath, request.url));
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)", // Skip all internal paths (_next)
+    "/", // Match the root path
+  ],
+};
